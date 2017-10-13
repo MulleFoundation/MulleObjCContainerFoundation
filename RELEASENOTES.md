8a59e5b14e0277de6be2c274ca03c52ac751d032
Nat! <nat@codeon.de>
2017-10-13 16:20:45 +0200

    * migrated from mulle-homebrew to mulle-project

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,3 +1,8 @@
+### 0.12.1
+
+* migrated from mulle-homebrew to mulle-project
+* adapt to MulleObjC 0.12.2
+
 # 0.9.1
 
 * keep version in step with MulleObjC for now (will diverge later)
@@ -6,5 +11,3 @@
 # 0.2.0
 
 * Start of versioning. Keep this version number in sync with MulleObjC!
-
-
---
+++ mulle-project/version-info.sh
@@ -0,0 +1,9 @@
+# -- Version Info --
+#
+# Keep these commented out, if the automatic detection works well
+# enough for you. If you don't have this file, there will be
+# not git operations.
+#
+VERSIONNAME="MULLE_OBJC_VERSION"
+VERSIONFILE="../MulleObjC/src/ns_rootconfiguration.h"
+VERSIONIMMUTABLE="YES"
